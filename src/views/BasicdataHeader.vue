ab7e9c3819db0f9b9c7b7b6a8f2b86d0c409185d
unknown <1534520586@qq.com>
2026-04-15 15:35:00 +0800

    --save

--- src/views/BasicdataHeader.vue
+++ src/views/BasicdataHeader.vue
@@ -1,11 +1,20 @@
 <template>
     <div>
-    </div>
+     <el-menu :default-active="activeIndex"
+  class="el-menu-demo"
+  mode="horizontal"
+  @select="handleSelect">
+  <el-menu-item index="1">基础数据维护</el-menu-item>
+  <el-sub-menu index="2"> 暂无</el-sub-menu>
+</el-menu>
+</div>
 </template>
-
 <script setup>
     import {ref,reactive} from 'vue'
+    import {useRouter} from 'vue-router'
+    import loyat from './loyat.vue';
 </script>
 
 <style scoped>
+
 </style>
